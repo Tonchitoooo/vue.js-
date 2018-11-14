d172f5cc91ac5de008b0b9b9712b2ab938af8476
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-15 00:29:43 +0300

    Create App.vue

---
+++ 46_localDirective/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2 v-colored:background.font="'green'">{{ title }}</h2>
+    <h2 v-colored:color.delay.font="'blue'">{{ title }}</h2>
+
+    <h2 v-font>Local font directive</h2>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        title: 'Hello I am Vue',
+        visible: true
+      }
+  },
+  directives: {
+      font:{
+        bind(el, bindings, vnode){
+          el.style.fontSize = '40px'
+        }
+      }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>

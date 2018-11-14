57bbc5aacf1bc8c6cad02ee150143f019da134f1
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-14 22:46:03 +0300

    Create Car.vue

---
+++ 38_isolutionStyle/Car.vue
@@ -0,0 +1,21 @@
+<template>
+ <div>
+   <h2>Car</h2>
+     <p>Nine little nigger boys sat up very late;
+       One overslept himself, and then there were eight</p>
+ </div>
+</template>
+
+<script>
+
+  export default {
+
+  }
+
+</script>
+
+<style scoped>
+ div{
+   border: 1px solid black;
+ }
+</style>

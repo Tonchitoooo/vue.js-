929c85a60065deceff7dc0184a3f2c6122ee946f
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-14 21:23:12 +0300

    Create App.vue

---
+++ 35_parametrChoise/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+  <h1> Parent: {{ carName }}</h1>
+
+  <app-car v-bind:carName="carName"
+                 :carYear="carYear"
+                 :changeFunc ="changeNameToAudi"
+                 @nameChanged="carName = $event"
+  ></app-car>
+  </div>
+</template>
+
+<script>
+  import Car from './Car.vue'
+
+export default {
+  data() {
+    return {
+      carName: 'Ford',
+      carYear: 2018
+    }
+  },
+  methods:{
+    changeNameToAudi() {
+      this.carName = 'Audi'
+    }
+  },
+  components:{
+    AppCar: Car
+  }
+}
+</script>
+
+<style>
+
+</style>

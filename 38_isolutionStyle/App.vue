16c840fe67d1e56231b4ad387b94762a76276675
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-14 22:45:42 +0300

    Create App.vue

---
+++ 38_isolutionStyle/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <app-counter></app-counter>
+    <app-car></app-car>
+  </div>
+</template>
+
+<script>
+  import Car from './Car.vue'
+  import Counter from './Counter.vue'
+
+export default {
+  components:{
+    AppCar: Car,
+    AppCounter: Counter
+  }
+}
+</script>
+
+<style>
+
+</style>

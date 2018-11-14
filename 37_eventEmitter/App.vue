6e13580bdade9f7de0f8ed892c8a00ba59e3071e
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-14 22:30:11 +0300

    Create App.vue

---
+++ 37_eventEmitter/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+  <h1> Parent: {{ carName }}</h1>
+
+    <app-counter></app-counter>
+    <!--<app-counter :counter="counter"></app-counter>-->
+
+  <app-car v-bind:carName="carName"
+                 :carYear="carYear"
+                 :changeFunc ="changeNameToAudi"
+                 @nameChanged="carName = $event"
+                 @counterUpdated="counter = $event"
+
+  ></app-car>
+  </div>
+</template>
+
+<script>
+  import Car from './Car.vue'
+  import Counter from './Counter.vue'
+
+export default {
+  data() {
+    return {
+      carName: 'Ford',
+      carYear: 2018,
+      //counter: 0
+    }
+  },
+  methods:{
+    changeNameToAudi() {
+      this.carName = 'Audi'
+    }
+  },
+  components:{
+    AppCar: Car,
+    AppCounter: Counter
+  }
+}
+</script>
+
+<style>
+
+</style>

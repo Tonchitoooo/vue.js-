9ef8383747262a9a4e10c3b455ad2f9855a571b8
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-14 22:46:20 +0300

    Create Counter.vue

---
+++ 38_isolutionStyle/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <h2>Counter</h2>
+  <p> Nine little nigger boys sat up very late;
+    One overslept himself, and then there were eight  </p>
+</div>
+</template>
+
+<script>
+
+  export default {
+
+  }
+</script>
+
+<style scoped>
+  h2{
+    color: rebeccapurple;
+  }
+</style>

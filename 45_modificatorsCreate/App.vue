cc7bf215f1df9b664b317ee655827974c5e15324
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-15 00:23:30 +0300

    Create App.vue

---
+++ 45_modificatorsCreate/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2 v-colored:background.font="'green'">{{ title }}</h2>
+    <h2 v-colored:color.delay.font="'blue'">{{ title }}</h2>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        title: 'Hello I am Vue',
+        visible: true
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

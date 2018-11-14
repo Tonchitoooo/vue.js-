14cd39c765948f074119d98667e87c48dcccf11f
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-14 23:45:13 +0300

    Create App.vue

---
+++ 44_argInDirective/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2 v-colored:background="'green'">{{ title }}</h2>
+    <h2 v-colored:color="'blue'">{{ title }}</h2>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        title: 'Hello I am Vue',
+        visible: true
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

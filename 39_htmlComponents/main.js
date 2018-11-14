6f77e0ced3db464e7a65112ecd9d0efa9d8e2552
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-14 23:04:04 +0300

    Create main.js

---
+++ 39_htmlComponents/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import Car from './Car.vue'
+//
+// Vue.component('app-car',Car)
+
+export const eventEmitter = new Vue()
+
+new Vue({
+  el: '#app',
+   render: h => h(App)
+  //    render: function (h) {
+  //       return h(App)
+  //    }
+});
+
+

ae884a09fece150872ca80ab1b9dbc6244ebb2a2
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-14 23:03:24 +0300

    Create App.vue

---
+++ 39_htmlComponents/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <app-car>
+      <h2 slot="title">{{ carName }}</h2>
+      <p slot="name">Lorem ipsum dolor.</p>
+    </app-car>
+  </div>
+</template>
+
+<script>
+  import Car from './Car.vue'
+
+
+export default {
+    data() {
+      return {
+        carName: 'Ford'
+      }
+    },
+  components:{
+    AppCar: Car
+  }
+}
+</script>
+
+<style scoped>
+  h2 {
+    color: aqua;
+  }
+
+</style>

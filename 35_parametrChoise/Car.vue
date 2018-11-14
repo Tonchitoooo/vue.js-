c4fa1e9d3b9f8f3bd80329e090ab79ba70df11ef
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-14 21:23:30 +0300

    Create Car.vue

---
+++ 35_parametrChoise/Car.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="car">
+    <h3> Name: {{ carName }}\ {{ reverseName }}</h3>
+    <p>Year: {{ carYear }}</p>
+    <button @click="changeName">Change Name</button>
+    <button @click="changeFunc">Change From Parent</button>
+  </div>
+</template>
+
+<script>
+
+  export default {
+   // props:['carName', 'carYear'],
+   //  props: {
+   //    carName: String,
+   //    carYear: Number
+   //  },
+    props: {
+      carName:{
+        type: String,
+        required: true
+      } ,
+      carYear: Number,
+      changeFunc: Function
+    },
+    methods: {
+      changeName() {
+        this.carName = "Mazda"
+        this.$emit('nameChanged', this.carName)
+      }
+    },
+    computed: {
+      reverseName() {
+        return this.carName.split('').reverse().join('')
+      }
+    }
+  }
+
+</script>
+
+<style>
+  .car{
+    border: 1px solid black ;
+  }
+</style>

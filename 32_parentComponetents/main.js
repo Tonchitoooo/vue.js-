d3ed15178929f6b0d784d702b5c1d51c31838794
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-14 18:48:43 +0300

    Create main.js

---
+++ 32_parentComponetents/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+// import Car from './Car.vue'
+//
+// Vue.component('app-car',Car)
+
+new Vue({
+  el: '#app',
+   render: h => h(App)
+  //    render: function (h) {
+  //       return h(App)
+  //    }
+})

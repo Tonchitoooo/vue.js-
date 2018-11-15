e1a3ea0e11fbf080cf947e2577a1c2d2a4dd4a1b
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-15 22:29:06 +0300

    Create App.vue

---
+++ 56_myControl/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Form inputs</h2>
+    <app-Onoff v-model="switched"></app-Onoff>
+
+    <div>
+      <h3 v-if="switched"> Component is enabled</h3>
+      <h3 v-else> Component is disabled</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Onoff from './Onoff.vue'
+
+  export default {
+
+    data() {
+      return{
+        switched: false
+      }
+    },
+
+    components: {
+      appOnoff: Onoff
+    }
+  }
+
+</script>

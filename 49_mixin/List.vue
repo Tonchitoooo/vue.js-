90b8053a39c3544beb126c642e63f59f3131edbe
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-15 20:36:24 +0300

    Create List.vue

---
+++ 49_mixin/List.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div>
+    <h1>list</h1>
+    <input type="text" v-model="searchName">
+    <ul>
+      <li v-for="name of filteredNames">{{name}}</li>
+    </ul>
+    <hr>
+  </div>
+</template>
+
+<script>
+  import ListMixin from './listMixin'
+  export default {
+    mixins: [ListMixin]
+  }
+</script>

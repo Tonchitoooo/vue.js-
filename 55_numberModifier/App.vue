3299faca2a0a22a01f80e8d1d0d66aae7ae1ffd6
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-15 22:02:53 +0300

    Create App.vue

---
+++ 55_numberModifier/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h2>Form inputs</h2>
+
+    <input type="text" v-model.number="age">
+
+    <p>{{age}}</p>
+
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    data() {
+      return {
+       age: 20
+      }
+    },
+    watch: {
+      age( value){
+        console.log(value)
+          console.log(typeof value)
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+  textarea {
+    width: 300px;
+    height: 100px;
+  }
+  p {
+    white-space: pre;
+  }
+</style>

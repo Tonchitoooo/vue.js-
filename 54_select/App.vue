2518f04820f8d4f0cfdd1afbb5f9e1eda05a332c
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-15 21:56:45 +0300

    Create App.vue

---
+++ 54_select/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>Form inputs</h2>
+
+    <select v-model="social">
+      <option v-for="s in socialList"> {{ s }}</option>
+    </select>
+
+    <hr>
+    <p>{{social}}</p>
+
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    data() {
+      return {
+        dafaultSocial: 'Facebook',
+        social: 'Instagram',
+        socialList:['Instagram', 'Vk', 'Facebook']
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+  textarea {
+    width: 300px;
+    height: 100px;
+  }
+  p {
+    white-space: pre;
+  }
+</style>

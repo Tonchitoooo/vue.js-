8619255ec82b70754798987f7c67e62a71740976
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-15 21:30:26 +0300

    Create App.vue

---
+++ 51_textarea/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Form inputs</h2>
+    <textarea v-model="textarea"></textarea>
+    <p>{{textarea}}</p>
+
+    <p>{{ name }}</p>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    data() {
+      return {
+        textarea: 'Initial state'
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+  textarea {
+    width: 300px;
+    height: 100px;
+  }
+  p {
+    white-space: pre;
+  }
+</style>

a1ede7098bebadda36f4750c6ad3993d810c5c21
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-15 21:45:45 +0300

    Create App.vue

---
+++ 53_radio/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>Form inputs</h2>
+    <label>
+      <input type="radio" value="Instagram" v-model="social"> Instagram
+    </label>
+
+    <label>
+      <input type="radio" value="Vk" v-model="social"> Vk
+    </label>
+
+    <label>
+      <input type="radio" value="Facebook" v-model="social"> Facebook
+    </label>
+
+    <hr>
+
+
+      <li >{{ social }}</li>
+   
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    data() {
+      return {
+        social: ''
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+  textarea {
+    width: 300px;
+    height: 100px;
+  }
+  p {
+    white-space: pre;
+  }
+</style>

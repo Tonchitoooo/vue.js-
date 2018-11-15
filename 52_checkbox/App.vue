6e87b24ccfee7a0e1d79b06a263c796c4a4993eb
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-15 21:39:39 +0300

    Create App.vue

---
+++ 52_checkbox/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h2>Form inputs</h2>
+    <label>
+      <input type="checkbox" value="Instagram" v-model="social"> Instagram
+    </label>
+
+    <label>
+      <input type="checkbox" value="Vk" v-model="social"> Vk
+    </label>
+
+    <label>
+      <input type="checkbox" value="Facebook" v-model="social"> Facebook
+    </label>
+
+    <hr>
+
+    <ul>
+      <li v-for="s in social">{{ s }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    data() {
+      return {
+        social: []
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+  textarea {
+    width: 300px;
+    height: 100px;
+  }
+  p {
+    white-space: pre;
+  }
+</style>

8a2e345fc3ed2704c094626c7db2f5efd2f000dc
Tonchitoooo <37882222+Tonchitoooo@users.noreply.github.com>
2018-11-16 12:43:01 +0300

    Update and rename 63_66_routing/routes.js to 63_74_routing/routes.js

---
+++ 63_74_routing/routes.js
@@ -0,0 +1,72 @@
+import VueRouter from 'vue-router'
+import Home from './pages/Home'
+import Carfull from './pages/Carfull'
+import ErrorCmp from './pages/ErrorCmp'
+
+const Cars = resolve => {
+  require.ensure(['./pages/Cars.vue'], ()=> {
+    resolve(
+      require('./pages/Cars.vue')
+    )
+  })
+}
+const Car = resolve => {
+  require.ensure(['./pages/Car.vue'], ()=> {
+    resolve(
+      require('./pages/Car.vue')
+    )
+  })
+}
+
+export default  new VueRouter({
+  routes: [
+    {
+      path: '',
+      component: Home
+    },
+    {
+      path: '/cars',
+      component: Cars,
+      name:'cars'
+    },
+    {
+      path: '/car/:id',
+      component: Car,
+      children: [
+        {
+          path:'full',
+          component: Carfull,
+          name: 'carFull',
+          beforeEnter(to, from, next) {
+            console.log('beforeEnter')
+            next()
+          }
+        }
+      ]
+    },
+    {
+      path: '/none',
+      redirect: '/cars'
+    },
+    {
+      path: '*',
+      component: ErrorCmp
+    }
+  ],
+  mode: 'history',
+  scrollBehavior (to, from, savedPosition) {
+
+    if (savedPosition) {
+      return savedPosition
+    }
+
+    if(to.hash) {
+      return{ selector: to.hash}
+    }
+    return {
+      x: 0,
+      y: 0
+    }
+  }
+
+})
